c80c073d50e02534cf08fbc12a3ec29cb69d3276
rsterenchak <rsterenchak@gmail.com>
2026-03-06 07:52:18 +0000

    Initial

---
+++ tutorials/numbers.cs
@@ -0,0 +1,27 @@
+WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+}
